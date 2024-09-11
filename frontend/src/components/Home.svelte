1a36025386d92f64cbc19568a334c2e13af5a7b6
Ethan Olsen <131eolsen@gmail.com>
2024-09-11 02:42:12 -0700

    feat: rename home, add init styles

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -13,7 +13,7 @@
 	};
 </script>
 
-<div class="flex flex-col items-center h-full">
+<div class="flex flex-col items-center h-full w-full">
 	<div class="prose container mx-auto px-4 w-full py-6">
 		<h1>Welcome to Lowky Youtube-dl UI</h1>
 		<!-- FORM -->
@@ -22,7 +22,7 @@
 				<span class="sr-only">Width</span>
 				<span
 					aria-hidden
-					class="absolute left-5 top-4 text-xxs text-muted-foreground">URL</span
+					class="absolute left-3 top-4 text-xxs text-muted-foreground">URL</span
 				>
 				<input
 					class="h-input w-full rounded-10px border border-border-input bg-background pl-10 pr-2 text-sm text-foreground"
@@ -39,6 +39,36 @@
 	</div>
 	<!-- INFO -->
 	{#if info !== null}
-		<pre class="max-w-md">{JSON.stringify(info, null, 2)}</pre>
+		<div class="container mx-auto flex flex-col gap-2 w-full px-4">
+			<div class="grid sm:grid-cols-2 gap-2">
+				<img
+					class="rounded-card-lg shadow-md md:col-span-1"
+					alt={info.title}
+					srcset={info.thumbnails?.map((t) => `${t.URL} ${t.Width}w`).join(",")}
+					sizes={info?.thumbnails
+						?.map((t, i) =>
+							i === (info?.thumbnails?.length ?? 1) - 1
+								? `${t.Width}px`
+								: `(max-width: ${t.Width * 2}px) ${t.Width}px`,
+						)
+						.join(",")}
+				/>
+				<div class="md:col-span-1 flex flex-col gap-1">
+					<h2 class="text-4xl">{info.title}</h2>
+					<p>{info.author}</p>
+					<p>{info.duration}</p>
+					<p>{info.time}</p>
+				</div>
+			</div>
+			<div class="flex flex-col gap-1">
+				{#each info.qualityInfo as quality}
+					<div
+						class="rounded-card border border-muted bg-background-alt p-3 shadow-card"
+					>
+						{JSON.stringify(quality)}
+					</div>
+				{/each}
+			</div>
+		</div>
 	{/if}
 </div>
