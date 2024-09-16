f1d99bfc977d6e97e6fd1d12724fee5996f97fc8
Ethan Olsen <131eolsen@gmail.com>
2024-09-16 14:57:36 -0700

    feat: recheck ffmpeg

--- frontend/src/components/InstallFFMPEG.svelte
+++ frontend/src/components/InstallFFMPEG.svelte
@@ -18,10 +18,22 @@
 		<p>
 			If this process does not work please visit <a
 				href="https://www.ffmpeg.org/"
-				target="_blank">ffmpeg.org</a
-			> and restart this application.
+				target="_blank"
+			>
+				ffmpeg.org
+			</a> and restart this application.
 		</p>
 
+		<Button.Root
+			disabled={downloading}
+			on:click={recheckFFMPEG}
+			type="button"
+			class="mx-auto inline-flex h-12 items-center justify-center rounded-input bg-dark
+			px-[21px] text-[15px] font-semibold text-background shadow-mini
+			hover:bg-dark/95 active:scale-98 active:transition-all disabled:opacity-50 disabled:cursor-not-allowed"
+		>
+			Recheck FFMPEG
+		</Button.Root>
 		<Button.Root
 			disabled={downloading}
 			on:click={install}
