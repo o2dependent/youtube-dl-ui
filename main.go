dfe24375629dfb1650c834c4559b79a0ff572f63
Ethan Olsen <131eolsen@gmail.com>
2024-09-19 05:22:38 -0700

    feat: min width and min height

--- main.go
+++ main.go
@@ -25,6 +25,8 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		MinWidth:         360,
+		MinHeight:        360,
 		Bind: []interface{}{
 			app,
 		},
